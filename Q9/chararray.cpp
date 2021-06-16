d9cd40862ed68c08e8c6d1bb56c006c263d26ab7
Faraz <farazattari786@gmail.com>
2021-06-16 10:26:32 +0530

    Using for loops in character array

---
+++ Q9/chararray.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    char str[100];
+    char alphabets[50];
+
+    cout << "Input Message: ";
+    cin.getline(str, 100);
+
+    // Printing message
+    cout << "Message: " << str << endl;
+
+    // Counting number of symbols
+    int times = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        bool appears = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (str[j] == str[i])
+            {
+                appears = true;
+                break;
+            }
+        }
+
+        if (!appears)
+        {
+            times++;
+        }
+    }
+    cout << "Number of symbols in the alphabet = " << times << endl;
+
+    // Printing characters
+    cout << "Characters in the alphabet = ";
+    int unique = 0;
+    int indalp = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        bool appears = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (str[j] == str[i])
+            {
+                appears = true;
+                break;
+            }
+        }
+
+        if (!appears)
+        {
+            cout << str[i] << ", ";
+            alphabets[indalp] = str[i];
+            ;
+            unique++;
+            indalp++;
+        }
+    }
+
+    // Printing number of bits per symbol
+    cout << "\nNumber of bits per symbol = " << ceil(log2(times)) << endl;
+}
