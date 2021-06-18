5f34aeb7e05b16e060455fd4779f8d81021bd2ae
Faraz <farazattari786@gmail.com>
2021-06-18 11:27:43 +0530

    added histogram and removed the last comma from characters

--- Q9/chararray.cpp
+++ Q9/chararray.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int main()
 {
-    char str[100];
+    char str[100] = "2020cccbbba";
     char alphabets[50];
 
-    cout << "Input Message: ";
-    cin.getline(str, 100);
+    // cout << "Input Message: ";
+    // cin.getline(str, 100);
 
     // Printing message
     cout << "Message: " << str << endl;
@@ -54,9 +54,12 @@ int main()
 
         if (!appears)
         {
-            cout << str[i] << ", ";
+            cout << str[i];
+            if (i != strlen(str) - 1)
+            {
+                cout << ", ";
+            }
             alphabets[indalp] = str[i];
-            ;
             unique++;
             indalp++;
         }
@@ -64,4 +67,19 @@ int main()
 
     // Printing number of bits per symbol
     cout << "\nNumber of bits per symbol = " << ceil(log2(times)) << endl;
+
+    // Printin Histogram
+    cout << "Histogram showing the frequency of the symbols in the alphabet\n";
+    for (int i = 0; i < unique; i++)
+    {
+        cout << alphabets[i] << " | ";
+        for (int j = 0; j < strlen(str); j++)
+        {
+            if (alphabets[i] == str[j])
+            {
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
 }
