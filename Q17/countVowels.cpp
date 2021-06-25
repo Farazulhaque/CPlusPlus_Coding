05ff5a16fdb835e9912f6914a547021dd815d6bd
Faraz <farazattari786@gmail.com>
2021-06-25 09:24:04 +0530

    added sorting of characters in alphabetical order using for loop

--- Q17/countVowels.cpp
+++ Q17/countVowels.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_CHAR = 26;
+
 int main()
 {
+    int charCount[MAX_CHAR] = {0};
     string name;
     int count = 0;
     cout << "Enter First name and last name: ";
@@ -14,8 +17,13 @@ int main()
         {
             count++;
         }
+        charCount[name[i] - 'a']++;
     }
     cout << "Number of vowels: " << count;
+    cout << "Characters in alphabetical order: ";
+    for (int i = 0; i < MAX_CHAR; i++)
+        for (int j = 0; j < charCount[i]; j++)
+            cout << (char)('a' + i);
     return 0;
 }
 
