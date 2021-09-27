e3ad6d8a2758cd7e1716c02f5487023a2c017252
Farazulhaque <farazattari786@gmail.com>
2021-09-27 06:18:02 +0530

    math expressions

---
+++ Q42/expression.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x, y, z, a, b, d, g, y;
+
+    g = (x + 2) / pow(z, 2);
+
+    y = pow(x, 2) / pow(z, 2);
+
+    a = (pow(x, 2) + pow(y, 3) + pow(z, 4)) / (pow(a + b, 2) - d);
+    
+    
+    return 0;
+}
