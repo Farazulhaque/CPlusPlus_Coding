215dbf8b269844d798dcf7a3144bc7f7a6a427f1
Farazulhaque <farazattari786@gmail.com>
2021-10-12 18:59:06 +0530

    math expressions

---
+++ Q47/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 2, y = 5, z = 6;
+    // Q1
+    cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
+    cout << "x + y = " << x + y << endl;
+    cout << "Sum of " << x << " and " << z << " is " << x + z << endl;
+    cout << "z / x = " << z / x << endl;
+    cout << "2 times " << x << " = " << 2 * x << endl;
+    return 0;
+}
